e956202e4cc0c39668517450225882f63f84a06b
Antonio Ramos <ramos.antonio.dev@gmail.com>
2021-12-21 18:47:48 -0300

    Exercicios cuorse dia 4.1/parte6

--- m1-fundamentals/b4-introducao-a-javascript-e-logica-de-programacao/4.1-javascript-primeiros-passos/scripts5.js
+++ m1-fundamentals/b4-introducao-a-javascript-e-logica-de-programacao/4.1-javascript-primeiros-passos/scripts5.js
@@ -146,17 +146,6 @@ console.log(lucro);
 
 //11-Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
 // A notação para um salário de R$1500,10, por exemplo, deve ser 1500.10. Para as faixas de impostos, use as seguintes referências:
-// INSS (Instituto Nacional do Seguro Social)
-// Salário bruto até R$ 1.556,94: alíquota de 8%
-// Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%
-// Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11%
-// Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88
-// IR (Imposto de Renda)
-// Até R$ 1.903,98: isento de imposto de renda
-// De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
-// De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
-// De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
-// Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
 // Exemplo: Uma pessoa possui o salário bruto de R$ 3.000,00. O cálculo será:
 // O salário bruto está entre R$ 2.594,93 e R$ 5.189,82, então sua alíquota para INSS é de 11%. O INSS será 11% de R$ 3.000, ou seja, R$ 330,00.
 // Para descobrir o salário-base, subtraia do salário bruto a alíquota do INSS: R$ 3.000,00 - R$ 330,00 = R$ 2.670,00.
@@ -168,14 +157,6 @@ console.log(lucro);
 // O último cálculo para conseguir o salário líquido é R$ 2.670,00 - R$ 57,45 (salário-base - valor IR) = R$ 2.612,55.
 // Resultado: R$ 2.612,55.
 // Dica: que tal identificar as alíquotas com variáveis de nomes explicativos?
-
-const salarioBruto = 5000.00;
-const salarioBaseINSS = salarioBruto;
-const salarioBaseIR = salarioBaseINSS - descontoINSS;
-let descontoINSS;
-let descontoIR;
-let salarioLiquido;
-
 
 // INSS (Instituto Nacional do Seguro Social)
 // Salário bruto até R$ 1.556,94: alíquota de 8%
@@ -189,3 +170,34 @@ let salarioLiquido;
 // De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
 // De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
 // Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
+
+const salarioBruto = 5000.00;
+const salarioBaseINSS = salarioBruto;
+let descontoINSS;
+let descontoIR;
+
+if (salarioBaseINSS <= 1556.94) {
+  descontoINSS = salarioBaseINSS * 0.08;
+} else if (salarioBaseINSS <= 2594.92) {
+  descontoINSS = grossSalary * 0.09;
+} else if (salarioBaseINSS <= 5189.82) {
+  descontoINSS = salarioBaseINSS * 0.11;
+} else {
+  descontoINSS = 570.88;
+}
+
+const salarioBaseIR = salarioBaseINSS - descontoINSS;
+
+if (salarioBaseIR <= 1903.98) {
+  descontoIR = 0;
+} else if (salarioBaseIR <= 2826.65) {
+  descontoIR = (salarioBaseIR * 0.075) - 142.80;
+} else if (salarioBaseIR <= 3751.05) {
+  descontoIR = (salarioBaseIR * 0.15) - 354.80;
+} else if (salarioBaseIR <= 4664.68) {
+  descontoIR = (salarioBaseIR * 0.225) - 636.13;
+} else {
+  descontoIR = (salarioBaseIR * 0.275) - 869.36;
+};
+
+console.log(`Salário Líquido: ${(salarioBaseIR - descontoIR)}`);
