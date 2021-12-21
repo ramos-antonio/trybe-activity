d2294de05c009cf13cd060921375791edb18d59a
Antonio Ramos <ramos.antonio.dev@gmail.com>
2021-12-21 12:22:49 -0300

    Exercicios cuorse dia 4.1/parte2

---
+++ m1-fundamentals/b4-introducao-a-javascript-e-logica-de-programacao/4.1-javascript-primeiros-passos/scripts1.js
@@ -0,0 +1,20 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge);
+
+//Área e perímetro de um retângulo de base 5 e altura 8
+const base = 5;
+let height = 8;
+const area = base * height
+console.log("Área do Retangulo = " + area);
+const perimetro = (base * 2) + (height * 2)
+console.log("Perimetro do Retangulo = " + perimetro);
