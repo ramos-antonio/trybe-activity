5d3f5ff477f9a33dca479e276f7a8e734d11d878
Antonio Ramos <ramos.antonio.dev@gmail.com>
2021-12-21 12:05:20 -0300

    Exercicios cuorse dia 4.1/parte1

---
+++ m1-fundamentals/b4-introducao-a-javascript-e-logica-de-programacao/4.1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,8 @@
+const myName = "Antonio";
+const birthCity = "Salvador";
+let birthYear = 1980;
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+birthYear = 2030;
+console.log(birthYear);
