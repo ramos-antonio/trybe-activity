a477527df2b1c29479cb50d6755d15e0d91ce94b
Antonio Ramos <ramos.antonio.dev@gmail.com>
2021-12-21 12:33:40 -0300

    Exercicios cuorse dia 4.1/parte3

---
+++ m1-fundamentals/b4-introducao-a-javascript-e-logica-de-programacao/4.1-javascript-primeiros-passos/scripts2.js
@@ -0,0 +1,12 @@
+//Estruturas condicionais com operadores if/else
+
+const nota = 100;
+  if (nota >= 80) {
+    console.log("Parabéns, você foi aprovada(o)!");
+  }
+  else if (nota >= 60 && nota < 80) {
+    console.log("Você está na nossa lista de espera");
+  }
+  else {
+    console.log("Você foi reprovada(o)");
+  }
