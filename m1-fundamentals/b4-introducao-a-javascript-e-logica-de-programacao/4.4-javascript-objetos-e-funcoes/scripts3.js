54abe3bcd2da3c7b1469264ea61b211a2db42585
Antonio Ramos <ramos.antonio.dev@gmail.com>
2021-12-24 18:55:31 -0300

    Exercicios cuorse dia 4.4/parte4

---
+++ m1-fundamentals/b4-introducao-a-javascript-e-logica-de-programacao/4.4-javascript-objetos-e-funcoes/scripts3.js
@@ -0,0 +1,61 @@
+//1-Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+// function operacaoAritmetica(a, b) {
+// }
+
+// const a = 4;
+// const b = 2;
+
+// console.log(`Adição (a + b) = ${a + b}`);
+// console.log(`Subtração (a - b) = ${a - b}`);
+// console.log(`Multiplicação (a * b) = ${a * b}`);
+// console.log(`Divisão (a / b) = ${a / b}`);
+// console.log(`Módulo (a % b) = ${a % b}`);
+
+//2-Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+function maiorNum(c, d) {
+  if (c > d) {
+    console.log(`Valor de "c = ${c}" é maior que o valor de "d = ${d}"`);
+  } else {
+    console.log(`Valor de "c = ${c}" é menor que o valor de "d = ${d}"`);
+  }
+}
+maiorNum(10, 3)
+
+
+//3-Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+function maiorDeTres(e, f, g) {
+  if (e > f && e > g) {
+    console.log(`Valor de "e = ${e}" é maior que o valor de "f = ${f}" e "g = ${g}`);
+  } else if (f > e && f > g) {
+    console.log(`Valor de "f = ${f}" é maior que o valor de "e = ${e}" e "g = ${g}`);
+  } else {
+    console.log(`Valor de "g = ${g}" é maior que o valor de "e = ${e}" e "f = ${f}`);
+  }
+}
+maiorDeTres(10, 5, 20)
+
+//4-Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+function positivoNegativo(h) {
+  if (h > 0) {
+    console.log(`Valor de "h = ${h}" é Positivo`);
+  } else if (h < 0) {
+    console.log(`Valor de "h = ${h}" é Negativo`);
+  } else {
+    console.log(`Valor de "h = ${h}" é Zero`);
+  }
+}
+positivoNegativo(0)
+
+// //5-Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+// const anguloLadoA = 60;
+// const anguloLadoB = 60;
+// const anguloLadoC = 60;
+// const somaDosLados = anguloLadoA + anguloLadoB + anguloLadoC
+
+// if ((anguloLadoA + anguloLadoB + anguloLadoC) === 180 && anguloLadoA > 0 && anguloLadoB > 0 && anguloLadoC > 0) {
+//   console.log(true);
+// } else if (anguloLadoA <= 0 || anguloLadoB <= 0 || anguloLadoC <= 0) {
+//   console.log("erro");
+// } else {
+//   console.log(false);
+// }
