8c2b11c8d0efff5707cf7972850da69a05b8d854
Antonio Ramos <ramos.antonio.dev@gmail.com>
2021-12-24 15:00:15 -0300

    Exercicios cuorse dia 4.3/parte1

--- m1-fundamentals/b4-introducao-a-javascript-e-logica-de-programacao/4.3-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ m1-fundamentals/b4-introducao-a-javascript-e-logica-de-programacao/4.3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -1,7 +1,7 @@
 //Utilizando o array abaixo, percorra-o somando todos os valores. Caso o valor final seja maior que 15, imprima-o. Caso seja igual ou menor que 15, imprima a mensagem: "valor menor que 16":
 
 let fruits = [3, 4, 10, 1, 12];
-fruits.sort((a, b) => a - b);
+// fruits.sort((a, b) => a - b);
 let soma = 0;
 for (let index = 0; index < fruits.length; index += 1) {
   soma += fruits[index];
