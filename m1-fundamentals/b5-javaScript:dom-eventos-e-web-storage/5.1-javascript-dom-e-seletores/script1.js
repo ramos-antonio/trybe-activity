1ee9fbdd8fadebdab122fd05580524c069a85f99
Antonio Ramos <ramos.antonio.dev@gmail.com>
2021-12-25 20:22:12 -0300

    Exercicios cuorse dia 5.1/parte2

---
+++ m1-fundamentals/b5-javaScript:dom-eventos-e-web-storage/5.1-javascript-dom-e-seletores/script1.js
@@ -0,0 +1,7 @@
+const paragraph = document.getElementsByClassName("paragraph");
+// console.log(paragraph);
+paragraph[0].style.backgroundColor = "yellow";
+// console.log(paragraph[0]);
+const h4 = document.getElementsByTagName("h4");
+console.log(h4);
+h4[0].style.color = "red";
