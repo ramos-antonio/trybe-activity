2ae51933193625af2da48e9003545aaebeb6b4e4
Antonio Ramos <ramos.antonio.dev@gmail.com>
2021-12-25 15:22:16 -0300

    Exercicios cuorse dia 4.4/ParteI-Objetos e For/In

--- m1-fundamentals/b4-introducao-a-javascript-e-logica-de-programacao/4.4-javascript-objetos-e-funcoes/scripts3.js
+++ m1-fundamentals/b4-introducao-a-javascript-e-logica-de-programacao/4.4-javascript-objetos-e-funcoes/scripts3.js
@@ -1,15 +1,31 @@
 //1-Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:
-// function operacaoAritmetica(a, b) {
-// }
-
 // const a = 4;
 // const b = 2;
+function soma(a, b) {
+  return a + b;
+}
+console.log(`Adição (a + b) = ${soma(4, 2)}`);
 
-// console.log(`Adição (a + b) = ${a + b}`);
-// console.log(`Subtração (a - b) = ${a - b}`);
-// console.log(`Multiplicação (a * b) = ${a * b}`);
-// console.log(`Divisão (a / b) = ${a / b}`);
-// console.log(`Módulo (a % b) = ${a % b}`);
+function subtracao(a, b) {
+  return a - b;
+}
+console.log(`Subtração (a - b) = ${subtracao(4, 2)}`);
+
+function multiplicacao(a, b) {
+  return a * b;
+}
+console.log(`Multiplicação (a * b) = ${multiplicacao(4, 2)}`);
+
+function divisao(a, b) {
+  return a / b;
+}
+console.log(`Divisão (a / b) = ${divisao(4, 2)}`);
+
+function modulo(a, b) {
+  return a % b;
+}
+console.log(`Módulo (a % b) = ${modulo(4, 2)}`);
+
 
 //2-Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
 function maiorNum(c, d) {
@@ -47,15 +63,13 @@ function positivoNegativo(h) {
 positivoNegativo(0)
 
 // //5-Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
-// const anguloLadoA = 60;
-// const anguloLadoB = 60;
-// const anguloLadoC = 60;
-// const somaDosLados = anguloLadoA + anguloLadoB + anguloLadoC
-
-// if ((anguloLadoA + anguloLadoB + anguloLadoC) === 180 && anguloLadoA > 0 && anguloLadoB > 0 && anguloLadoC > 0) {
-//   console.log(true);
-// } else if (anguloLadoA <= 0 || anguloLadoB <= 0 || anguloLadoC <= 0) {
-//   console.log("erro");
-// } else {
-//   console.log(false);
-// }
+function validadorDeTriangulo(anguloLadoA, anguloLadoB, anguloLadoC) {
+  if ((anguloLadoA + anguloLadoB + anguloLadoC) === 180 && anguloLadoA > 0 && anguloLadoB > 0 && anguloLadoC > 0) {
+    console.log(true);
+  } else if (anguloLadoA <= 0 || anguloLadoB <= 0 || anguloLadoC <= 0) {
+    console.log("erro");
+  } else {
+    console.log(false);
+  }
+}
+validadorDeTriangulo(60, 60, 60)
---
+++ m1-fundamentals/b4-introducao-a-javascript-e-logica-de-programacao/4.4-javascript-objetos-e-funcoes/scripts4.js
@@ -0,0 +1,109 @@
+//Parte I - Objetos e For/In
+//Usando o objeto abaixo, faça os exercícios a seguir:
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+//1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
+//Bem-vinda, Margarida
+function msgBoasVindas() {
+  let name = info.personagem;
+  console.log(`Bem vinda, ${name}!`);
+}
+msgBoasVindas();
+//2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+/*
+{
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim'
+}
+*/
+info.recorrente = "sim"
+console.log(info);
+
+//3 - Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+/*
+personagem
+origem
+nota
+recorrente
+*/
+for (const key in info) {
+  console.log(key);
+}
+
+//4 - Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+/*
+Margarida
+Pato Donald
+Namorada do personagem principal nos quadrinhos do Pato Donald
+Sim
+*/
+for (const key in info) {
+  console.log(info[key]);
+}
+
+//5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
+/*
+Margarida e Tio Patinhas
+Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
+Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
+Ambos recorrentes // Atenção para essa última linha!
+*/
+let info1 = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "sim",
+};
+for (let properties in info) {
+  if (
+    properties === 'recorrente' &&
+    info[properties] === 'Sim' &&
+    info1[properties] === 'Sim'
+  ) {
+    console.log(`Ambos recorrentes`);
+  } else {
+    console.log(`${info[properties]} e ${info1[properties]}`);
+  }
+}
+
+//Usando o objeto abaixo, faça os exercícios a seguir:
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+
+//6 - Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama '${leitor.livrosFavoritos[0].titulo}'`);
+
+//7 - Adicione um novo livro favorito na chave livrosFavoritos , que é um array . Atribua a esta chave um objeto contendo as seguintes informações:
+/*
+{
+  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+  autor: 'JK Rowling',
+  editor: 'Rocco',
+}
+*/
+leitor.livrosFavoritos.push(
+  {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editor: 'Rocco',
+  },
+);
+console.log(leitor);
+
+//8 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
+console.log(`${leitor.nome} tem ${leitor.livrosFavoritos.length} livros favoritos`);
