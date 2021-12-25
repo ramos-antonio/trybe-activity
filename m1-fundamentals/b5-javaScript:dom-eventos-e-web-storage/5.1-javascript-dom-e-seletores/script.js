8258d8b853851bf9eb03cfe11ce3ea2556860b7b
Antonio Ramos <ramos.antonio.dev@gmail.com>
2021-12-25 19:49:25 -0300

    Exercicios cuorse dia 5.1/parte1

---
+++ m1-fundamentals/b5-javaScript:dom-eventos-e-web-storage/5.1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,14 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+const pageTitle = document.getElementById("page-title");
+pageTitle.innerHTML = "Poeira em alto mar";
+
+const secondParagraph = document.getElementById("second-paragraph");
+secondParagraph.innerHTML = "<strong>Lorem</strong> ipsum dolor sit amet, consectetur adipisicing elit. Accusamus quas ullam corrupti et ut! Itaque nobis nam vero dolor possimus voluptatem, ipsum perspiciatis placeat ratione laudantium, facilis quos. Ipsa, non.";
+secondParagraph.style.fontFamily = "sans";
+secondParagraph.style.backgroundColor = "yellow";
+
+const subtitle = document.getElementById("subtitle");
+subtitle.innerHTML = "Sinopse";
+subtitle.style.fontSize = "20px";
