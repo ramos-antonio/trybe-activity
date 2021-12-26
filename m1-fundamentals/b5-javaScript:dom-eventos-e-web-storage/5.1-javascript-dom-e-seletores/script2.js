e2bd0d5f454e3e8c57138a8c9bed8b200f56bcea
Antonio Ramos <ramos.antonio.dev@gmail.com>
2021-12-26 20:39:57 -0300

    Exercicios cuorse dia 5.1/parte3

---
+++ m1-fundamentals/b5-javaScript:dom-eventos-e-web-storage/5.1-javascript-dom-e-seletores/script2.js
@@ -0,0 +1,21 @@
+const header = document.querySelectorAll("#header-container");
+// console.log(header);
+header[0].style.backgroundColor = "rgb(0, 176, 105)";
+
+const div = document.querySelectorAll("div");
+// console.log(div);
+div[0].style.backgroundColor = "red";
+div[1].style.backgroundColor = "yellow";
+div[2].style.backgroundColor = "orange";
+div[3].style.backgroundColor = "green";
+
+const h3 = document.querySelectorAll("h3");
+// console.log(h3);
+h3[0].style.backgroundColor = "black";
+h3[1].style.backgroundColor = "black";
+h3[2].style.backgroundColor = "black";
+h3[3].style.backgroundColor = "black";
+
+const footer = document.querySelectorAll("#footer-container");
+// console.log(footer);
+footer[0].style.backgroundColor = "rgb(0, 53, 51)";
