0463cb09d56287beb1aeaefddb9accf373589a5b
Antonio Ramos <ramos.antonio.dev@gmail.com>
2022-01-04 18:07:47 -0300

    Exercicios cuorse dia 7.3/Parte1

---
+++ m1-fundamentals/b7-introducao-a-javascript-es6-e-testes-unitarios/7.3-primeiros-passos-em-jest/script.js
@@ -0,0 +1,6 @@
+//------------------------------------NodeJS Assert------------------------------------//
+
+const assert = require('assert'); // Sintaxe para incluir o módulo assert
+
+assert.strictEqual(50, 50); // Sem erros: 50 === 50
+assert.strictEqual(50, 70); // AssertionError: 50 !== 70
